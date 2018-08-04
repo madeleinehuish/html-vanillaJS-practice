c97d79f4ead9f4990cf180547fd288aa47ff77d6
Madeleine Huish <madeleinehuish@gmail.com>
2018-08-04 12:47:43 -0700

    added some practice html files

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,33 +1,97 @@
 //make search bar save value and print it to the screen somewhere
 
 let table = document.getElementById('tableId');
+let tableBody = document.getElementById('tableBody');
 let showElem = document.getElementById('showElem');
 let tableColumns = document.querySelectorAll('td');
+let tableElems = document.querySelectorAll('td');
+
+let clearButton = document.getElementById('clearButton');
+let searchInput = document.getElementById('testInputBox');
+let tableInput = document.getElementById('tableInput');
+
 for(let column of tableColumns) {
 	column.addEventListener('click', click, false);
 }
-let clearButton = document.getElementById('clearButton');
+
+for(let td of tableElems) {
+	td.addEventListener('mouseover', hover, false);
+	td.addEventListener('mouseout', hoverExit, false);
+}
+
 clearButton.addEventListener('click', clear, false);
 
-let searchInput = document.getElementById('testInputBox');
 searchInput.addEventListener('input', event => {
-	console.log(event.target.value);
+	console.log('searchInput: ',event.target.value);
 	let inputContainer = document.getElementById('testInput');
 	inputContainer.innerHTML = '<div>' + event.target.value + '</div>';
 	// inputContainer.style.border = '2px solid black'
 	inputContainer.className = 'testInputClass';
 }, false);
 
+tableInput.addEventListener('input', event => {
+	console.log('tableInput: ',event.target.value);
+	if(event.target.value==='') {
+		for(let td of tableElems) {
+			console.log('row: ', td);
+			td.style.backgroundColor = '#ecf5f3';
+		}
+	}
+	let found = null;
+	let from = null;
+	let tableNumbersNumbers = [null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+	let tableNumbersWords = [null,'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen'];
+	if (tableNumbersNumbers.includes(Number(event.target.value)) || tableNumbersWords.includes(event.target.value)) {
+			if(tableNumbersNumbers.includes(Number(event.target.value))) found = Number(event.target.value);
+			else found = tableNumbersWords.indexOf(event.target.value);
+
+			if(found > 11) from = found - 10;
+			else if (found > 5) from = found - 5;
+			else from = found;
+
+			let foundParent = Math.ceil(found/5)-1;
+
+			let foundElem = tableBody.children[foundParent].children[from-1];
+			foundElem.style.backgroundColor = 'green';
+			// console.log('foundElem', foundElem);
+			// let chosenTableElement = Array.from(tableBody.children)
+			// 															.map((element, index) => ({element, index}))
+			// console.log(chosenTableElement)
+	}
+
+
+
+})
+
+function hover(e) {
+	let target = e.target;
+	console.log('target: ', target);
+	target.style.backgroundColor = '#f1d2fc';
+}
+
+function hoverExit(e) {
+	let target = e.target;
+	console.log('target: ', target);
+	target.style.backgroundColor = '#ecf5f3';
+}
 
 function click(e) {
 	let target = e.target;
 	let newDiv = '<div>' + e.target.textContent + '</div>';
 	showElem.innerHTML = newDiv;
 	console.log(target);
-
 }
 
 function clear() {
 	let newDiv = '<div></div>';
 	showElem.innerHTML = newDiv;
+	for(let td of tableElems) {
+		console.log('row: ', td);
+		td.style.backgroundColor = '#ecf5f3';
+	}
+	tableInput.value = '';
 }
+
+
+//1. find nth element from search bar
+//2. do a complex node with text, span and locate something inside
--- src/public/js/jqueryIndex.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  // jQuery goes here...
-
-  for(let i=0; i < 25; i++) {
-    $("#homeSpan").delay(200).fadeOut().fadeIn('slow');
-  }
-
-});
