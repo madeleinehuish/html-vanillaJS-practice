b22acee8a9415259c31413b126359b9135255d6d
Madeleine Huish <madeleinehuish@gmail.com>
2018-09-20 20:17:25 -0700

    latest

--- server2.js
+++ server2.js
@@ -1,13 +1,14 @@
-const port = 9000;
 const express = require("express");
 
 const app = express();
 const bodyParser = require('body-parser');
 
+const port = 9000;
+
 app.use(bodyParser.urlencoded({ extended: true })); //need this for regular form elements
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + "/src/practice")); //use static files in ROOT/src folder
+app.use(express.static(__dirname + "/src/practice")); //use static files in ROOT/src/practice folder
 
 app.post("/forms", (request, response) => { //root dir
 	console.log('Form received!!!');
